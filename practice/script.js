e245b02519306ccb15747634b75bb570d1ef0879
ljk4268 <ljk4268@kakao.com>
2022-06-20 22:39:26 +0900

    페이지 끝 체크해서 데이터 추가로 가져오기

--- practice/script.js
+++ practice/script.js
@@ -4,6 +4,10 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  let page = 1
+  const limit = 10
+  const end = 100
+  let total = 10
   
   //fetch - response 객체
   //await - promise 객체
@@ -13,7 +17,8 @@
 
   // async 비동기 함수 실행할거라는 의미 
   const getPost = async () => {
-    const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+    // 데이터를 몇개씩 가져올지 정해주는 코드 
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
   // 원래 비동기 함수를 통해 얻어지는 데이터값은 promise라는 객체로 감싸짐 
   // 실제 비동기함수가 실행되는 코드는 아래 코드. 
   // await를 써야지 비동기함수가 작업을 완료했을 때 promise객체를 벗긴 값을 할당해줌
@@ -29,6 +34,7 @@
   }
 
   const $posts = get('.posts')
+  const $loader = get('.loader')
 
   const showPosts = (posts)=>{
     posts.forEach((post)=>{
@@ -45,14 +51,46 @@
     })
   }
 
+  const showLoader = ()=>{
+    $loader.classList.add('show')
+  }
+
+  const hideLoader = ()=>{
+    $loader.classList.remove('show')
+  }
   const loadPost = async () => {
+    // 로딩 엘리먼트를 보여줌 
+    showLoader()
+    try{
     const response = await getPost()
     showPosts(response)
+    } catch (error) {
+      console.error(error.message)
+    } finally {
+      // 로딩 엘리먼트를 사라지게함
+      hideLoader()
+    }
+  }
+
+  const onScroll = () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement
+
+    if( total == end ){
+      window.removeEventListener('scroll', onScroll)
+      return
+    }
+    if ( scrollTop + clientHeight >= scrollHeight - 5 ){
+      page++
+      total += 10
+      loadPost()
+    }
   }
 
   /* 데이터 불러오는 과정 - end */
   // DOMContentLoaded 이벤트 발생시 안에있는 코드들 실행되게 함
   window.addEventListener('DOMContentLoaded', ()=>{
     loadPost()
+    // scoll이벤트가 감지될 때 onScroll함수가 실행되게 해줌 
+    window.addEventListener('scroll', onScroll)
   })
 })()
